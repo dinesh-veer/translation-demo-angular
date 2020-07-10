3d2629aa0885f7360ca649aeff1f82ae5b5b4002
dineshveer <dveer123@hotmail.com>
2020-07-10 19:50:41 +0530

    Added file size in screen

--- translation-demo/src/app/app.component.ts
+++ translation-demo/src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-import { TranslateService} from '@ngx-translate/core'
+import { Component, VERSION } from '@angular/core';
+import { TranslateService} from '@ngx-translate/core';
+import {HttpClientModule, HttpClient, HttpRequest, HttpResponse, HttpEventType, HttpHeaders} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,11 +8,94 @@ import { TranslateService} from '@ngx-translate/core'
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  constructor(private translate: TranslateService) {
+  percentDone: number;
+  uploadSuccess: boolean;
+
+  constructor(private translate: TranslateService, private http: HttpClient) {
  //console.log(translate.getBrowserLang());
     translate.setDefaultLang(translate.getBrowserLang());
 }
 useLanguage(language: string) {
   this.translate.use(language);
 }
+
+size:String;
+version = VERSION
+  
+  upload(files: File[]){
+    //pick from one of the 4 styles of file uploads below
+    this.uploadAndProgress(files);
+  }
+
+  basicUpload(files: File[]){
+    var formData = new FormData();
+    const headers = new HttpHeaders()
+      .append('Content-Type', 'application/json')
+      .append('Access-Control-Allow-Headers', 'Content-Type')
+//      .append('Access-Control-Allow-Methods', 'GET')
+      .append('Access-Control-Allow-Origin', '*');
+      console.log(files);
+    Array.from(files).forEach(f => formData.append('file', f))
+    this.http.post('https://file.io', formData,{headers})
+      .subscribe(event => {  
+        console.log('done')
+      })
+  }
+  
+  //this will fail since file.io dosen't accept this type of upload
+  //but it is still possible to upload a file with this style
+  basicUploadSingle(file: File){    
+    this.http.post('https://file.io', file)
+      .subscribe(event => {  
+        console.log('done')
+      })
+  }
+  
+  /**
+     * Format the size to a human readable string
+     *
+     * @param bytes
+     * @returns the formatted string e.g. `105 kB` or 25.6 MB
+     */
+     formatBytes(bytes: number): string {
+      const UNITS = ['Bytes', 'kB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+      const factor = 1024;
+      let index = 0;
+
+      while (bytes >= factor) {
+        bytes /= factor;
+        index++;
+      }
+
+      return `${parseFloat(bytes.toFixed(2))} ${UNITS[index]}`;
+    }
+
+  uploadAndProgress(files: File[]){
+    console.log(files)
+    this.size=this.formatBytes(files[0].size);
+    var formData = new FormData();
+    Array.from(files).forEach(f => formData.append('file',f))
+    
+    this.http.post('https://file.io', formData, {reportProgress: true, observe: 'events'})
+      .subscribe(event => {
+        if (event.type === HttpEventType.UploadProgress) {
+          this.percentDone = Math.round(100 * event.loaded / event.total);
+        } else if (event instanceof HttpResponse) {
+          this.uploadSuccess = true;
+        }
+    });
+  }
+  
+  //this will fail since file.io dosen't accept this type of upload
+  //but it is still possible to upload a file with this style
+  uploadAndProgressSingle(file: File){    
+    this.http.post('https://file.io', file, {reportProgress: true, observe: 'events'})
+      .subscribe(event => {
+        if (event.type === HttpEventType.UploadProgress) {
+          this.percentDone = Math.round(100 * event.loaded / event.total);
+        } else if (event instanceof HttpResponse) {
+          this.uploadSuccess = true;
+        }
+    });
+  }
 }
